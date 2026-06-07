01edc1c8d7035711b8a3124a96c911c2d6e75b71
FaizanKhan-AutoDev <mfaizankhan7711@gmail.com>
2026-06-07 23:03:04 +0100

    Added seed command with sample locations - Added sensemap seed management command (python manage.py seed) - Inserted 5 sample campus locations with sensory ratings - Clear existing locations first so re-running is safe"

---
+++ sensemap/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import Location
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    """Turns a Location into JSON (and validates incoming JSON).
+
+    Add new fields to `fields` when you add them to the model.
+    """
+
+    class Meta:
+        model = Location
+        fields = [
+            "id",
+            "name",
+            "description",
+            "category",
+            "latitude",
+            "longitude",
+            "auditory",
+            "visual",
+            "olfactory",
+            "thermal",
+            "vestibular",
+            "is_quiet_zone",
+        ]
