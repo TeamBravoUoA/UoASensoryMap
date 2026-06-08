69ca4a65ec2a0b40582dbdce1aabf03374e385cc
FaizanKhan-AutoDev <mfaizankhan7711@gmail.com>
2026-06-08 01:01:11 +0100

    added admin setting to control the database and users from the interface

--- sensemap/admin.py
+++ sensemap/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "is_quiet_zone", "auditory", "visual")
+    list_filter = ("category", "is_quiet_zone")
+    search_fields = ("name", "description")
