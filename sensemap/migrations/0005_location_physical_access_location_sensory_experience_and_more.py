fc5387dc434bcbdde94ec0932cb276b6af4d41a1
Elorm Agbledzorwu <enochagbledzorwu@gmail.com>
2026-07-16 22:39:32 +0100

    migration file
    Resolving migration file conflict

--- sensemap/migrations/0005_location_physical_access_location_sensory_experience_and_more.py
+++ sensemap/migrations/0005_location_physical_access_location_sensory_experience_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sensemap', '0005_alter_location_category'),
+        ('sensemap', '0004_location_slug'),
     ]
 
     operations = [
