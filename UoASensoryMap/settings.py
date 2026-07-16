d14530f611e977167905be472b2916d6abe82524
FaizanKhan-AutoDev <mfaizankhan7711@gmail.com>
2026-07-16 20:04:03 +0100

    Fix media serving in production and dotenv import

--- UoASensoryMap/settings.py
+++ UoASensoryMap/settings.py
@@ -14,15 +14,12 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from dotenv import load_dotenv
+from python_dotenv import load_dotenv
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "sensemap" / "media"
 
 
 # Quick-start development settings - unsuitable for production
